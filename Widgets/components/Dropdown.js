d58f6bddd6c47f3433e8e7ca6ec802aa1013d749
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-06-16 21:14:29 +0530

    upload

--- Widgets/components/Dropdown.js
+++ Widgets/components/Dropdown.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Dropdown = (props) => {
-   return <h1>Dropdown</h1>;
+const Dropdown = ({options,selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false);
+   
+    const renderedOptions = options.map((option) =>{
+        if(option.value === selected.value){
+            return null;
+        }
+        return (
+            <div 
+              key={option.value} 
+              className="item" 
+              onClick={() => onSelectedChange(option)}
+            >
+                {option.label}
+            </div>
+        );
+    });
+
+   return ( 
+       <div className="ui form">
+           <div className="field">
+               <label className="lable">Select a Color</label>
+               <div
+                  onClick={() => setOpen(!open)} 
+                  className={`ui selection dropdown ${open ? 'visible active': ''}`}
+                >
+                   <i className="dropdown icon"></i>
+                   <div className="text">
+                       {selected.label}
+                   </div>
+                   <div className={`menu ${open ? 'visible transition' : ''}`}>
+                       {renderedOptions}
+                   </div>
+               </div>
+           </div>
+       </div>
+   );
 };
 
 export default Dropdown;
