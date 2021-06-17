4f618269d7677270cfa7c6de44c3f1b665da8a83
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-06-17 11:49:35 +0530

    remove body listener

--- Widgets/components/Dropdown.js
+++ Widgets/components/Dropdown.js
@@ -5,14 +5,21 @@ const Dropdown = ({options,selected, onSelectedChange}) => {
     const ref = useRef();
 
     useEffect(() => {
-        document.body.addEventListener('click',(event) => {
+        const onBodyClick = (event) => {
             if(ref.current.contains(event.target)){
                 return;
             }
 
             setOpen(false);
-        });
+        }
+        document.body.addEventListener('click',onBodyClick);
+
+        return () =>{
+            document.body.removeEventListener('click',onBodyClick);
+        };
+        
     },[]);
+
     const renderedOptions = options.map((option) =>{
         if(option.value === selected.value){
             return null;
