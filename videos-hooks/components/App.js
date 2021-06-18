5b8942819be86ac707a07037d8894dbbb0aef922
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-06-18 16:43:42 +0530

    change to class to function of hooks

--- videos-hooks/components/App.js
+++ videos-hooks/components/App.js
@@ -1,56 +1,57 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
 
-class App extends React.Component{
-    //<VideoDetail video={this.state.selectedVideo} />
-    state = { videos: [], selectedVideo: null };
+const App = () => {
 
-    componentDidMount(){
-      this.onTermSubmit('flowers');
-    }
+  const [videos,setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-    onTermSubmit = async (term) =>{
-      //console.log(term);
-      const response = await youtube.get('/search',{
-          params:{
-            q:term
-          }
-      });
-       
-      //console.log(response);
-      this.setState({
-        videos: response.data.items,
-        selectedVideo: response.data.items[0]
-      });
-    };
+  useEffect(() => {
+    onTermSubmit('flowers');
+  },[]);
 
-    onVideoSelect = (video) =>{
-        this.setState({selectedVideo: video });
-        //console.log(video.snippet.title);
-    };
+  //<VideoDetail video={this.state.selectedVideo} />
+     
+  const onTermSubmit = async (term) =>{
+    //console.log(term);
+    const response = await youtube.get('/search',{
+        params:{
+          q:term
+        }
+     });
+     
+     
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
 
-    render(){
-        return (
-            <div className="ui container"> 
-              <SearchBar onSubmit={this.onTermSubmit} />
-              <div className="ui grid">
-                <div className="ui row">
-                  <div className="ten wide column">
-                     <VideoDetail video={this.state.selectedVideo} />
-                  </div>
-                  <div className="six wide column">
-                     <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-                   </div>
-                </div>
-              </div>
-              
-            </div>
-        );
-    }
-}
+  };
 
+    const onVideoSelect = (video) =>{
+       setSelectedVideo(video);
+       //console.log(video.snippet.title);
+   };
+
+   return (
+    <div className="ui container"> 
+      <SearchBar onSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="ten wide column">
+             <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="six wide column">
+             <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+           </div>
+        </div>
+      </div>
+      
+    </div>
+    ); 
+
+};
+ 
 export default App;
