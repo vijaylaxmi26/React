87b4462e74d01b16048962425702e05e063e7df5
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-06-18 19:10:26 +0530

    custom hooks

--- videos-hooks/components/App.js
+++ videos-hooks/components/App.js
@@ -1,50 +1,37 @@
 import React,{useState, useEffect} from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
-
+import useVideo from '../hooks/useVideos';
 
 const App = () => {
 
-  const [videos,setVideos] = useState([]);
+   
   const [selectedVideo, setSelectedVideo] = useState(null);
-
+  const [videos,search] = useVideo('React');
+  
   useEffect(() => {
-    onTermSubmit('flowers');
-  },[]);
+    setSelectedVideo(videos[0]);
+  },[videos]);
+   
 
   //<VideoDetail video={this.state.selectedVideo} />
-     
-  const onTermSubmit = async (term) =>{
-    //console.log(term);
-    const response = await youtube.get('/search',{
-        params:{
-          q:term
-        }
-     });
-     
-     
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-
-  };
-
-    const onVideoSelect = (video) =>{
-       setSelectedVideo(video);
-       //console.log(video.snippet.title);
-   };
+   
 
    return (
     <div className="ui container"> 
-      <SearchBar onSubmit={onTermSubmit} />
+      <SearchBar onSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="ten wide column">
              <VideoDetail video={selectedVideo} />
           </div>
           <div className="six wide column">
-             <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+             <VideoList 
+             //(video) => setSelectedVideo(video) = setSelectedVideo
+             onVideoSelect={setSelectedVideo} 
+             videos={videos} 
+             />
            </div>
         </div>
       </div>
