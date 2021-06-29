fd6c3c2a06139f79b6ca50bc27aab85e47b060b1
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-06-29 16:37:30 +0530

    initial file created

---
+++ Streams/Client/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BroweserRouter, Router, Link} from 'react-router-dom';
+
+const App = () => {
+    return (
+       <div>
+       App
+       </div>
+    );
+};
+
+export default App;
