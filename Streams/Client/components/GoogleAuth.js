a046af2d8fbbb36cb2706f6e2c8931e79c16a8a8
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-06-29 22:16:12 +0530

    sign in or out with google

--- Streams/Client/components/GoogleAuth.js
+++ Streams/Client/components/GoogleAuth.js
@@ -2,18 +2,40 @@ import React from 'react';
 
 class GoogleAuth extends React.Component{
 
+    state= {isSignedIn: null};
+
     componentDidMount(){
+
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                clientId: '185532324616-9md2o3f37mma3879slqg8tjttiasio1o.apps.googleusercontent.com',
                scope: 'email'
+            }).then(() => {
+                //get user logged in or not 
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn: this.auth.isSignedIn.get()})
             });
         });
          
     }
+
+    //console cammand for sign in or out
+    //gapi.auth2.getAuthInstance().signIn()
+    //gapi.auth2.getAuthInstance().signOut()
+
+    //helper method
+    renderAuthButton(){
+        if(this.state.isSignedIn === null){
+            return <div> i don't know</div>;
+        }else if(this.state.isSignedIn){
+            return <div>i am signed in</div>;
+        }
+
+        return <div>Im not signed in</div>;
+    }
     render(){
         return (
-            <div>GoogleAuth</div>
+            <div>{this.renderAuthButton()}</div>
         );
     }
 }
