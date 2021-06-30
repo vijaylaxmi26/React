89fca05a33f047df7319f8c29b3c0ccb83c67f19
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-06-30 18:12:38 +0530

    authentication done

--- Streams/Client/reducers/authReducer.js
+++ Streams/Client/reducers/authReducer.js
@@ -1,11 +1,12 @@
 const INTIAL_STATE = {
-    isSignedIn : null
+    isSignedIn : null,
+    userId: null
 };
 
 export default (state = INTIAL_STATE ,action) => {
    switch(action.type){
        case 'SIGN_IN':
-           return {...state,isSignedIn: true};
+           return {...state,isSignedIn: true,userId: action.payload};
         case 'SIGN_OUT':
             return {...state,isSignedIn: false};
         default:
