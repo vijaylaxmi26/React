952e583e91d97a078922df2c326e3eb99ceb8795
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-07-03 10:29:46 +0530

    edit link with id

--- Streams/Client/components/streams/StreamList.js
+++ Streams/Client/components/streams/StreamList.js
@@ -1,68 +1,62 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {fetchStreams} from '../../actions';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchStreams } from '../../actions';
 
-class StreamList extends React.Component{
-    componentDidMount(){
-        this.props.fetchStreams();
-    }
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
 
-    renderAdmin(stream){
-      if(stream.userId === this.props.currentUserId){
-          return  (
-              <div className="right floated content" >
-                  <button className="ui button primary">
-                      Edit
-                  </button>
-                  <button className="ui button negative">
-                      Delete
-                  </button>
-              </div>
-          );
-      }
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
     }
+  }
 
-    renderList(){
-        return this.props.streams.map(stream => {
-          return(
-              <div className="item" key={stream.id}>
-                  {this.renderAdmin(stream)}
-                  <i className="large middle aligned icon camera"/>
-                  <div className="content">
-                      {stream.title}
-                      <div className="discription">
-                          {stream.description}
-                      </div>
-                  </div>
-              </div>
-          );
-        });
-    }
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
 
-    renderCreate(){
-        if(this.props.isSignedIn){
-           return (
-               <div style={{textAlign: 'right'}}>
-                   <Link to="/streams/new" className="ui button primary">
-                         Create Stream
-                   </Link>
-               </div>
-           )
-        }
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
     }
-    render(){
-       // console.log(this.props.streams);
-        return  (
-            <div>
-               <h2>Streams</h2> 
-               <div className="ui celled list">
-                   {this.renderList()}
-               </div>
-               {this.renderCreate()}
-            </div>
-        );
-    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
+      </div>
+    );
+  }
 }
 
 //Object.values(state.streams) take object input and return same value array
