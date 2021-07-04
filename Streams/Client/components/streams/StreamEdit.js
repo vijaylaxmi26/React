6b85f3c07713fba073991dfa2db1fbcb11d31a53
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-07-04 21:48:40 +0530

    delete stream

--- Streams/Client/components/streams/StreamEdit.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const StreamEdit = (props) => {
-    console.log(props);
-    return <div>StreamEdit</div>;
-};
-
-export default StreamEdit;
--- Streams/Client/history.js
+++
@@ -1,6 +0,0 @@
-// import createHistory from 'history/createBrowserHistory';
-
-// export default createHistory();
-
-import { createBrowserHistory } from 'history'; 
-export default createBrowserHistory();
